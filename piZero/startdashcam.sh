674f683f309dbac874b93b05e0a22bd63c36072a
garzarobm <garzarobm@gmail.com>
2024-02-25 08:59:39 -0600

    add edits to quick start dashcam

--- piZero/startdashcam.sh
+++ piZero/startdashcam.sh
@@ -3,11 +3,16 @@
 #start upload file
 /bin/bash /home/piDash/piDash/piZero/dashcam.sh
 
-# remove files older than 3 days
+# remove files older than 3 days (259200 seconds) 
+#Use -mtime +3 to remove files older than 3 days
+#use -exec to remove the files
+#use {} to represent the files
+#use \; to end the command
+#uncomment if you want to remove files older than 3 days
 #find /home/pi/Desktop/DashVideos -type f -iname '*.flv' -mtime +3 -exec rm {} \;
 
 # start dashcam service 
-#sudo systemctl start dashcam
+sudo systemctl start dashcam
 
 
 
