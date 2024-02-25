ad3eec299682bb4451d2699bf7a4848895fc8f32
garzarobm <garzarobm@gmail.com>
2024-02-24 18:18:57 -0600

    add comments to shutdown script

--- piZero/shutdown.sh
+++ piZero/shutdown.sh
@@ -1,4 +1,22 @@
-#/bin/bash
+#/bin/bash /home/piDash/piDash/piZero/upload_files.sh
 
 # Shutdown the pi
 sudo shutdown now
+
+# now is the time to shutdown
+# here are some other options:
+# sudo shutdown -h now will shutdown and halt the pi
+# sudo shutdown -r now will shutdown and reboot the pi
+# sudo shutdown -h 0 will shutdown and halt the pi
+# sudo shutdown -r 0 will shutdown and reboot the pi
+# sudo shutdown -h 5 will shutdown and halt the pi in 5 minutes
+# sudo shutdown -r 5 will shutdown and reboot the pi in 5 minutes
+# sudo shutdown -c will cancel the shutdown
+# sudo shutdown -k will only send the warning message to all users
+# sudo shutdown -k 5 will only send the warning message to all users and shutdown in 5 minutes
+# sudo shutdown -k 5 "The system will shutdown in 5 minutes" will only send the warning message to all users and shutdown in 5 minutes
+# sudo shutdown -k 5 "The system will shutdown in 5 minutes" will only send the warning message to all users and shutdown in 5 minutes
+# sudo shutdown -k 5 "The system will shutdown in 5 minutes" will only send the warning message to all users and shutdown in 5 minutes
+# sudo shutdown -k 5 "The system will shutdown in 5 minutes" will only send the warning message to all users and shutdown in 5 minutes
+# sudo shutdown -k 5 "The system will shutdown in 5 minutes" will only send the warning message to all users and shutdown in 5 minutes
+# sudo shutdown -k 5 "The system will shutdown in 5 minutes" will only send the warning message to all users and shutdown in 5 minutes
